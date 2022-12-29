d97bf8b2c4a73ebe27b452be8b82addff5ec4633
mr nim <mrnim@nimtechnology.com>
2022-12-30 00:50:07 +0700

    add global_datastore

--- c2-03-redis.tf
+++ c2-03-redis.tf
@@ -114,6 +114,6 @@ resource "aws_cloudwatch_metric_alarm" "cache_memory" {
 resource "aws_elasticache_global_replication_group" "global_datastore" {
   count = var.global_datastore ? 1 : 0
 
-  global_replication_group_id_suffix = "lower(local.name)"
+  global_replication_group_id_suffix = lower(local.name)
   primary_replication_group_id       = aws_elasticache_replication_group.redis.id
 }
