afe59ee5a92b44840d1e532088ccde27832bcefa
mr nim <mrnim@nimtechnology.com>
2022-12-30 00:20:20 +0700

    add global_datastore

--- c2-03-local-value.tf
+++ c2-03-local-value.tf
@@ -2,10 +2,10 @@
 locals {
   owners = var.business_divsion
   environment = var.environment
-  name = "${var.business_divsion}-${var.environment}"
+  name = "redis-${var.business_divsion}-${var.environment}"
   common_tags = {
     owners = local.owners
     environment = local.environment
   }
-  redis_cluster_name = "redis-${var.business_divsion}-${var.environment}"
+  redis_cluster_name = "${var.global_datastore ? lower(local.name)-primary : lower(local.name)}"
 } 
