4fe984b44c772f63b25d99a35fc27586aa68027e
mr nim <mrnim@nimtechnology.com>
2023-01-03 01:23:45 +0700

    fix output of redis

--- c2-06-redis-output.tf
+++ c2-06-redis-output.tf
@@ -1,4 +1,5 @@
+# https://stackoverflow.com/questions/56257169/terraform-how-to-use-conditionally-created-resources-output-in-conditional-op
 output "global_replication_group_id" {
+  value = length(aws_elasticache_global_replication_group.global_datastore) > 0 ? aws_elasticache_global_replication_group.global_datastore[0].id : null
   description = "The suffix name of a Global Datastore"
-  value       = aws_elasticache_global_replication_group.global_datastore[0].id
 }
